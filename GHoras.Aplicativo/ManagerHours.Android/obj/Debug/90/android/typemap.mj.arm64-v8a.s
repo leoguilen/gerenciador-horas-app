275824e1fd026638c2e40b8b1d1288b2c84bed82
Leonardo Guilen <leonardoguilen1@gmail.com>
2020-01-21 22:18:39 -0300

    Deleted .gitignore file

---
+++ GHoras.Aplicativo/ManagerHours.Android/obj/Debug/90/android/typemap.mj.arm64-v8a.s
@@ -0,0 +1,26 @@
+	/* Data SHA1: 54400837ff5e24ac89a5e20a9393c6a233159c43 */
+	.arch	armv8-a
+	.file	"typemap.mj.inc"
+
+	/* Mapping header */
+	.section	.data.mj_typemap,"aw",@progbits
+	.type	mj_typemap_header, @object
+	.p2align	2
+	.global	mj_typemap_header
+mj_typemap_header:
+	/* version */
+	.word	1
+	/* entry-count */
+	.word	1521
+	/* entry-length */
+	.word	262
+	/* value-offset */
+	.word	145
+	.size	mj_typemap_header, 16
+
+	/* Mapping data */
+	.type	mj_typemap, @object
+	.global	mj_typemap
+mj_typemap:
+	.size	mj_typemap, 398503
+	.include	"typemap.mj.inc"
